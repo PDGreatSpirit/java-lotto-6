fd08f39f185d6f65baf454560acab1499b53e963
hiki31376 <psh980918@naver.com>
2024-04-12 09:23:49 +0900

    보너스 번호에 대한 테스트 코드(중복 , 범위 ) 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -11,7 +11,22 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7),23))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 번호에 중복된 숫자가 있지만 중복된 숫자를 제외해 사이즈가 같아도 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumberandSize() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5,6 ,6) , 10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있지만 중복된 숫자를 제외해 사이즈가 같아도 예외가 발생한다.")
+    @Test
+    void createLottoByRangeNumberandSize() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5,46) , 10))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -19,9 +34,25 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5),10))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("보너스 번호가 로또 번호안에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedBonusNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6),5))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("보너스 번호가 로또 번호안에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByRangeBonusNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6),46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
     // 아래에 추가 테스트 작성 가능
 }
